b996e379af73fb9d8f55d2efcc3aeafc05b84870
sodiadrhain <adesojiawobajo@gmail.com>
2020-12-26 00:53:10 +0100

    added loops

---
+++ Loops.java
@@ -0,0 +1,8 @@
+public class Loops {
+    //loop numbers lesser than 10
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++) {
+            System.out.println(i);
+        }
+     }
+}
