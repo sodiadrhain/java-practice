b600a0b9c32738e464de738d54d4b1c4ee47de14
sodiadrhain <adesojiawobajo@gmail.com>
2020-12-25 23:53:31 +0100

    added Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        helloworld();
+    }
+
+    static void helloworld() {
+        System.out.println("Hello World");
+    }
+}
